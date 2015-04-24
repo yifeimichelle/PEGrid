9dcb6ee158e85ed076553b630331d2a7d9c6e531
Cory Simon <corymsimon@barracuda.lbl.gov>
2015-04-24 10:46:10 -0700

    manual merge of forcefield.jl

--- src/forcefield.jl
+++ src/forcefield.jl
@@ -6,7 +6,7 @@ using DataFrames
 
 type Forcefield
     """
-    Store attributes of a forcefield
+    Stores attributes of a force field
     """
     name::String
 
@@ -28,52 +28,52 @@ type Forcefield
     mixingrules::String
     
     # constructor
-    Forcefield() = new()
-end
-    
-function constructforcefield(name::String, adsorbate::String; cutoff::Float64=12.5, mixingrules="Lorenz-Berthelot")
-    """
-    Return a Forcefield type filled in by importing force field file
-    """
-    # construct object
-    forcefield = Forcefield()
-    forcefield.name = name
-    forcefield.adsorbate = adsorbate
-    forcefield.cutoff = cutoff
-    forcefield.mixingrules = mixingrules
+    function Forcefield(name::String, 
+                        adsorbate::String; 
+                        cutoff::Float64=12.5, 
+                        mixingrules="Lorenz-Berthelot")
+        """
+        Importing force field file, fill atrributes
+        """
+        forcefield = new()
+        forcefield.name = name
+        forcefield.adsorbate = adsorbate
+        forcefield.cutoff = cutoff
+        forcefield.mixingrules = mixingrules
 
-    # read in pure X-X iteractions data
-    if ~ isfile("data/forcefields/" * name * ".csv")
-        @printf("Could not find file data/forcefields/%s.csv", name)
-    end
-    df = readtable("data/forcefields/" * name * ".csv", allowcomments=true)
-
-    # get adsorbate epsilon and sigma
-    if ~ (adsorbate in df[:atom])
-        error(@sprintf("Adsorbate %s not present in forcefield!", adsorbate))
-    end
-    adsorbate_eps = df[df[:atom] .== adsorbate, :][:epsilon][1]
-    adsorbate_sig = df[df[:atom] .== adsorbate, :][:sigma][1]
-    
-    # initialize arrays in forcefield
-    forcefield.ninteractions = size(df, 1)
-    forcefield.atoms = Array(String, forcefield.ninteractions)
-    forcefield.epsilon = zeros(Float64, forcefield.ninteractions)
-    forcefield.sigma = zeros(Float64, forcefield.ninteractions)
-
-    # compute and store adsorbate - X sigma/epsilon LJ params
-    for i = 1:forcefield.ninteractions
-        forcefield.atoms[i] = df[:atom][i]
-        if mixingrules == "Lorenz-Berthelot"
-            forcefield.epsilon[i] = sqrt(adsorbate_eps * df[:epsilon][i])
-            forcefield.sigma[i] = (adsorbate_sig + df[:sigma][i]) / 2.0
-        elseif mixingrules == "Surface"
-        	forcefield.epsilon[i] = df[:epsilon][i]
-        	forcefield.sigma[i] = df[:sigma][i]
-        else
-            error("These mixing rules are not implemented")
+        # read in pure X-X iteractions data
+        if ~ isfile("data/forcefields/" * name * ".csv")
+            @printf("Could not find file data/forcefields/%s.csv", name)
         end
-    end
-    
-    return forcefield
+        df = readtable("data/forcefields/" * name * ".csv", allowcomments=true)
+
+        # get adsorbate epsilon and sigma
+        if ~ (adsorbate in df[:atom])
+            error(@sprintf("Adsorbate %s not present in forcefield!", adsorbate))
+        end
+        adsorbate_eps = df[df[:atom] .== adsorbate, :][:epsilon][1]
+        adsorbate_sig = df[df[:atom] .== adsorbate, :][:sigma][1]
+        
+        # initialize arrays in forcefield
+        forcefield.ninteractions = size(df, 1)
+        forcefield.atoms = Array(String, forcefield.ninteractions)
+        forcefield.epsilon = zeros(Float64, forcefield.ninteractions)
+        forcefield.sigma = zeros(Float64, forcefield.ninteractions)
+
+        # compute and store adsorbate - X sigma/epsilon LJ params
+        for i = 1:forcefield.ninteractions
+            forcefield.atoms[i] = df[:atom][i]
+            if mixingrules == "Lorenz-Berthelot"
+                forcefield.epsilon[i] = sqrt(adsorbate_eps * df[:epsilon][i])
+                forcefield.sigma[i] = (adsorbate_sig + df[:sigma][i]) / 2.0
+            elseif mixingrules == "Surface"
+                forcefield.epsilon[i] = df[:epsilon][i]
+                forcefield.sigma[i] = df[:sigma][i]
+            else
+                error("These mixing rules are not implemented")
+            end
+        end
+        
+        return forcefield
+        end
 end
