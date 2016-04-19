3f5519b0a6d6f6c37895c14437830158767e865e
CorySimon <corysimon@berkeley.edu>
2016-04-19 15:25:50 -0700

    set up for users to use positiondistn. added message about data directory

--- src/PEGrid.jl
+++ src/PEGrid.jl
@@ -3,12 +3,15 @@ module PEGrid
 
 # this is where crystal structure files, adsorbate and forcefield input files are stored
 global const PEGRID_DATA_DIR = homedir() * "/Dropbox/PEGrid/data"
+@printf("Your data directory is set as:\n%s\n", PEGRID_DATA_DIR)
+@printf("Change the variable PEGRID_DATA_DIR in PEGrid.jl if you wish to change this\n")
  # global const PEGRID_OUTPUT_DIR = homedir() * "/PEGrid_output"
 
 include("henry.jl")
-include("gcmc.jl")
+ # include("gcmc.jl")
 include("energygrid.jl")
 include("getEwaldparams.jl")
+include("positiondistn.jl")
 export Framework, # framework.jl
        Adsorbate, uniform_unit_vector_on_sphere, random_rotation_matrix, # adsorbate.jl
        Forcefield, # forcefield.jl
@@ -18,5 +21,6 @@ export Framework, # framework.jl
        # energygrid.jl
        write_vdW_grid, write_electrostatic_grid, # Grid conflicts with Gadfly...
        getEwaldparams, # getEwaldparams.jl
-       grand_canonical_mc, adsorption_isotherm # gcmc.jl
+       grand_canonical_mc, adsorption_isotherm, # gcmc.jl
+       writeprobabilitydistncube # positiondistn.jl
 end
